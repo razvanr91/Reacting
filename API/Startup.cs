b9b58b6a32299db60a44537d46f649c93ddc5620
razvanr91 <razvanroman1991@gmail.com>
2021-09-10 12:38:37 +0100

    Add axios and semantic-ui-react

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,14 @@ namespace API
             {
                 options.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors( options => 
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +62,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
