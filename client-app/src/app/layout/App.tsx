13d2f96986fc3f6120eab9d7b33345b432736a26
razvanr91 <razvanroman1991@gmail.com>
2021-09-12 00:54:42 +0100

    Handle Create or Edit Activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,6 +7,8 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get<Activity[]>("https://localhost:5001/api/Activities").then(response => {
@@ -14,11 +16,44 @@ function App() {
     });
   }, []);
 
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(activity => activity.id === id))
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string){
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity) {
+    activity.id ? setActivities([...activities.filter(x => x.id != activity.id), activity])
+      : setActivities([...activities, activity]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
   return (
     <Fragment>
-      <NavBar />
+      <NavBar openForm={handleFormOpen} />
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard activities={activities} />
+        <ActivityDashboard 
+          activities={activities} 
+          selectedActivity={selectedActivity} 
+          selectActivity={handleSelectActivity} 
+          cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode} 
+          openForm={handleFormOpen} 
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
+        />
       </Container>
     </Fragment>
   );
