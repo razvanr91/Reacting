40b517c13663a91dbbcf45eaccd682a65db40898
razvanr91 <razvanroman1991@gmail.com>
2021-09-11 22:28:00 +0100

    Create folder structure and add Activity interface

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>("https://localhost:5001/api/Activities").then(response => {
+      setActivities(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Header as="h2" icon="users" content="Reacting" />
+      <List>
+        {activities.map(activity => (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
